403da7bbdc282c567d68c4c99f95b2aedf0bd454
Gustavo Scholze <scholzegustavo@gmail.com>
2022-01-18 23:21:37 -0300

    gan skeleton added

---
+++ 3DGAN.py
@@ -0,0 +1,17 @@
+""" GAN building blocks """
+
+import torch.nn as nn
+
+class Generator(nn.Module):
+    def __init__(self):
+        pass
+
+    def forward(self, z):
+        pass
+
+class Discriminator(nn.Module):
+    def __init__(self):
+        pass
+
+    def forward(self, x):
+        pass
