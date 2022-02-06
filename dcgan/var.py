a4ec3fb68bdbf923e5a2e0179c08754cd60c89f7
Gustavo Scholze <scholzegustavo@gmail.com>
2022-02-06 08:26:57 -0300

    New DCGAN training loop

--- dcgan/var.py
+++ dcgan/var.py
@@ -1,3 +1,5 @@
+import torch
+
 NUM_WORKERS = 2
 BATCH_SIZE = 16
 IMAGE_SIZE = 128
@@ -9,3 +11,4 @@ NUM_EPOCHS = 10
 LEARNING_RATE = 0.0002
 BETA1 = 0.5
 NGPU = 0
+DEVICE = torch.device("cuda:0" if (torch.cuda.is_available() and NGPU > 0) else "cpu")
