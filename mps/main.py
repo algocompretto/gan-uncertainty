2b3a373af91bded7a151478fbe7cd201f25ed891
Gustavo Scholze <scholzegustavo@gmail.com>
2022-02-09 21:20:00 -0300

    MPS results

---
+++ mps/main.py
@@ -0,0 +1,22 @@
+import numpy
+from PIL import Image
+from g2s import g2s
+import matplotlib.pyplot as plt
+
+# load data
+ti = numpy.array(Image.open('../training_images/O45V972I2D.png'));
+
+# QS call using G2S
+simulation, _ = g2s('-a', 'qs', '-ti', ti, '-di', numpy.zeros((200, 200)) * numpy.nan, '-dt', numpy.zeros((1,)), '-k',
+                    1.2, '-n', 50, '-j', 0.5);
+
+# Display results
+fig, (ax1, ax2) = plt.subplots(1, 2)
+fig.suptitle('Unconditional simulation')
+ax1.imshow(ti)
+ax1.set_title('Training image');
+ax1.axis('off');
+ax2.imshow(simulation)
+ax2.set_title('Simulation');
+ax2.axis('off');
+plt.show()
