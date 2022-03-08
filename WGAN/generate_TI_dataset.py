2043a37fd36a868416ede15b6a84dc91ef686e27
Gustavo Scholze <scholzegustavo@gmail.com>
2022-03-08 19:24:07 -0300

    formatted file

--- WGAN/generate_TI_dataset.py
+++ WGAN/generate_TI_dataset.py
@@ -1,3 +1,4 @@
+"""Creates dataset based on strebelle training image."""
 import math
 import os
 from typing import Tuple, Any, List
@@ -8,29 +9,33 @@ import numpy
 
 # r"C:\Users\gustavo.scholze\gan-for-mps\TI\strebelle.png"
 class DatasetCreator:
-    def __init__(self, ti_image_path: str, crop_size: int = 64, stride: int = 16) -> None:
+    """Creates dataset based on strebelle training image."""
+
+    def __init__(self, ti_image_path: str, crop_size: int = 64,
+                 stride: int = 16) -> None:
+        """Build constructor for DatasetCreator."""
         self.original_TI: List[numpy.uint8] = cv2.imread(ti_image_path)
         self.crop_size: int = crop_size
         self.stride: int = stride
 
     def complete_square_image(self, size: int = 64) -> Any:
-        """
-        Fills image with black stripes to complete the square image shape.
-        """
+        """Fill image with black stripes to complete the square image shape."""
         if self.original_TI.shape[0] < size:
-            strip = numpy.zeros((size - self.original_TI.shape[0], self.original_TI.shape[1], 3), dtype=numpy.int8)
+            strip = numpy.zeros(
+                (size - self.original_TI.shape[0],
+                 self.original_TI.shape[1], 3), dtype=numpy.int8)
             img = numpy.vstack((self.original_TI, strip))
 
         if self.original_TI.shape[1] < size:
-            strip = numpy.zeros((self.original_TI.shape[0], size - self.original_TI.shape[1], 3), dtype=numpy.int8)
+            strip = numpy.zeros(
+                (self.original_TI.shape[0],
+                 size - self.original_TI.shape[1], 3), dtype=numpy.int8)
             img = numpy.hstack((self.original_TI, strip))
 
         return img
 
     def yield_cropped_image(self) -> Tuple[int, int, Any]:
-        """
-        Generates crops for the input image.
-        """
+        """Generate crops for the input image."""
         height = self.original_TI.shape[0]
         width = self.original_TI.shape[1]
 
@@ -46,15 +51,15 @@ class DatasetCreator:
                 y_max = min(i * self.stride + self.crop_size, height)
                 cropped_image = self.original_TI[y_min:y_max, x_min:x_max]
 
-                if cropped_image.shape[0] != self.crop_size or cropped_image.shape[1] != self.crop_size:
-                    cropped_image = self.complete_square_image(cropped_image, self.crop_size)
+                if (cropped_image.shape[0] != self.crop_size or
+                        cropped_image.shape[1] != self.crop_size):
+                    cropped_image = self.complete_square_image(
+                        cropped_image, self.crop_size)
 
                 yield x_min, y_min, cropped_image
 
     def saves_sliding_windows(self) -> None:
-        """
-        Slides through image to save each square.
-        """
+        """Slides through image to save each square."""
         print("[INFO] Saving in sliding windows...")
         path = r"C:\Users\gustavo.scholze\gan-for-mps\TI_generated"
         try:
