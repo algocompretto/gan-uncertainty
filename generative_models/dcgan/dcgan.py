c444cf8c70cf537296592f84d068fa55250a031d
Gustavo Scholze <scholzegustavo@gmail.com>
2022-04-02 00:09:32 -0300

    fix: DCGAN images

--- generative_models/dcgan/dcgan.py
+++ generative_models/dcgan/dcgan.py
@@ -16,7 +16,7 @@ import torch
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--n_epochs", type=int, default=2000, help="number of epochs of training")
-parser.add_argument("--batch_size", type=int, default=10, help="size of the batches")
+parser.add_argument("--batch_size", type=int, default=64, help="size of the batches")
 parser.add_argument("--lr", type=float, default=0.0005, help="adam: learning rate")
 parser.add_argument("--b1", type=float, default=0.5, help="adam: decay of first order momentum of gradient")
 parser.add_argument("--b2", type=float, default=0.999, help="adam: decay of first order momentum of gradient")
@@ -24,7 +24,7 @@ parser.add_argument("--n_cpu", type=int, default=1, help="number of cpu threads 
 parser.add_argument("--latent_dim", type=int, default=100, help="dimensionality of the latent space")
 parser.add_argument("--img_size", type=int, default=128, help="size of each image dimension")
 parser.add_argument("--channels", type=int, default=1, help="number of image channels")
-parser.add_argument("--sample_interval", type=int, default=400, help="interval between image sampling")
+parser.add_argument("--sample_interval", type=int, default=100, help="interval between image sampling")
 parser.add_argument("--output_folder", type=str, default="images", help="output folder for all of the generated images")
 opt = parser.parse_args()
 print(opt)
