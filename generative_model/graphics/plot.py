214c8933d21d6a4c3f175f7107e299b3e9244c8b
algocompretto <scholzegustavo@gmail.com~>
2022-08-19 14:43:47 -0300

    :rocket: :hammer: Working workflow

---
+++ generative_model/graphics/plot.py
@@ -0,0 +1,306 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy
+from mpl_toolkits.axes_grid1 import ImageGrid
+import os
+import numpy as np
+import matplotlib as mpl
+import random
+
+
+def highlight_cell(x_coordinate, y_coordinate, axis=None, **kwargs):
+    rect = plt.Rectangle((x_coordinate - .5, y_coordinate - .5), 1, 1, fill=True, **kwargs)
+    axis = axis or plt.gca()
+    axis.add_patch(rect)
+    return rect
+
+def get_color_bar():
+    cmap = mpl.colors.ListedColormap(['white', "black"])
+
+    col_dict={0:"white", 1:"black"}
+    norm_bins = np.sort([*col_dict.keys()]) + 0.5
+    norm_bins = np.insert(norm_bins, 0, np.min(norm_bins) - 1.0)
+
+    norm = mpl.colors.BoundaryNorm(norm_bins, 2, clip=True)
+
+    labels = np.array(["Sandstone", "Shale"])
+    fmt = mpl.ticker.FuncFormatter(lambda x, pos: labels[norm(x)])
+
+    diff = norm_bins[1:] - norm_bins[:-1]
+    tickz = norm_bins[:-1] + diff / 2
+
+    colorbar = plt.colorbar(
+        mpl.cm.ScalarMappable(cmap=cmap, norm=norm),
+        ticks=tickz,
+        format=fmt,
+        spacing='proportional',
+        label='Facies',
+    )
+
+    return colorbar
+
+def etype_plot(dict_ti: pd.DataFrame):
+    _data = dict_ti.copy()
+
+    fig, ax = plt.subplots(1, 1, figsize=(5.5, 4))
+
+    # Get mean for each row
+    _data['etype'] = _data.mean(axis=1)
+    ax.imshow(_data['etype'].values.reshape(150, 150),
+              cmap='gray', origin='lower')
+
+    # Text settings
+    plt.title("Etype of SNESIM with generated TIs")
+    plt.xlabel("X coordinate")
+    plt.ylabel("Y coordinate")
+
+    # Colorbar settings
+    cmap = mpl.cm.gray
+    norm = mpl.colors.Normalize(vmin=0, vmax=1)
+    cb = plt.colorbar(mpl.cm.ScalarMappable(norm=norm, cmap=cmap),
+                      orientation='vertical', spacing="proportional")
+    cb.ax.tick_params()
+    cb.set_label("Mean")
+
+    # Saving images to .pdf format
+    plt.grid(False)
+    plt.savefig("data/results/etype.png", dpi=600, bbox_inches='tight')
+
+def calculate_uncertainty(dict_ti: pd.DataFrame):
+    _data = dict_ti.copy()
+
+    _data['prob_sand'] = _data.sum(axis=1)
+    _data['prob_shale'] = 100 - _data['prob_sand']
+
+    _data["u_max"] = _data[["prob_shale", "prob_sand"]].max(axis=1) / 100
+    _data["u_min"] = _data[["prob_shale", "prob_sand"]].min(axis=1) / 100
+
+    _data.drop('prob_sand', axis=1, inplace=True)
+    _data.drop('prob_shale', axis=1, inplace=True)
+
+    return _data
+
+def plot_uncertainty(dict_ti: pd.DataFrame):
+    _data = dict_ti.copy()
+
+    plt.figure(figsize=(5.5, 4))
+
+    _data = calculate_uncertainty(_data)
+
+    plt.imshow(_data['u_min'].values.reshape(150, 150),
+              cmap='gray', origin='lower')
+
+    plt.title("Simulation uncertainty from proposed workflow")
+    plt.xlabel("X coordinate")
+    plt.ylabel("Y coordinate")
+
+    # Colorbar settings
+    cmap = mpl.cm.gray
+    norm = mpl.colors.Normalize(vmin=0, vmax=1)
+    cb = plt.colorbar(mpl.cm.ScalarMappable(norm=norm, cmap=cmap),
+                 orientation='vertical', spacing="proportional")
+    cb.ax.tick_params()
+    cb.set_label("Uncertainty")
+
+    # Saving images to .pdf format
+    plt.grid(False)
+    plt.savefig("data/results/uncertainty.png", dpi=600, bbox_inches='tight')
+
+
+def plot_realizations_grid(real):
+    # Samples 25 TI for a grid of 5x5 images
+    rand_idx = [random.randint(0, 10) for _ in range(10)]
+    tis_sampled = [real[i] for i in rand_idx]
+
+    fig = plt.figure(figsize=(8, 5))
+
+    image_grid = ImageGrid(fig, 111,
+                           nrows_ncols=(3, 3),
+                           axes_pad=0.1,
+                           cbar_location="left",
+                           cbar_pad=0.15)
+
+    cb = get_color_bar()
+
+    fig.suptitle("SNESIM realizations from Generative model")
+
+    for ax, image in zip(image_grid, tis_sampled):
+        # Iterating over the grid returns the Axes.
+        ax.imshow(image, cmap="gray", origin="lower")
+    plt.savefig("data/results/simulation_grid.png", dpi=600, bbox_inches='tight')
+
+def get_sand_shale_proportion(image):
+    image = image.reshape(-1)
+    sand_prop = (len(np.where(image==1)[0]) / 150**2)*100
+    shale_prop = (len(np.where(image==0)[0]) / 150**2)*100
+    return sand_prop, shale_prop
+
+def proportions(real: numpy.ndarray) -> None:
+    fig, ax = plt.subplots(figsize=(5.5, 4))
+
+    sand_values = list()
+    shale_values = list()
+
+    # Get proportions for all
+    for i in range(1, len(real)):
+        sand, shale = get_sand_shale_proportion(real[i].reshape(-1))
+        sand_values.append(sand)
+        shale_values.append(shale)
+
+    df_dict = dict(Sand=sand_values, Shale=shale_values)
+    df = pd.DataFrame.from_dict(df_dict, orient='columns')
+
+    # Boxplot
+    bp = ax.boxplot(df, labels=["Sandstone", "Shale"], patch_artist=True,
+                    showmeans=True, showfliers=False)
+
+    for element in ['boxes', 'whiskers', 'fliers', 'means', 'medians', 'caps']:
+        plt.setp(bp[element], color="black")
+
+    for patch in bp['boxes']:
+        patch.set(facecolor='#f1f1f1')
+
+    plt.ylim(20, 80)
+
+    plt.ylabel('Percentage (%)')
+    plt.xlabel('Category')
+    plt.title('SNESIM Generated Training Images proportions')
+
+    plt.savefig("data/results/boxplot_snesim.png", dpi=600, bbox_inches='tight')
+
+def concatenate_out_files(directory: str):
+    cat_array = []
+    for im in os.listdir(directory):
+        image = np.loadtxt(directory+im, skiprows=4)[:,0]
+        cat_array.append(np.resize(image, (150, 150)))
+    return cat_array
+
+
+def proportions_comparison(real: numpy.ndarray, fake:numpy.ndarray) -> None:
+    fig, ax = plt.subplots(1, 2, figsize=(11, 6))
+
+    sand_values = list()
+    shale_values = list()
+
+    # Get proportions for real data
+    for i in range(1, len(real)):
+        sand, shale = get_sand_shale_proportion(real[i].reshape(-1))
+        sand_values.append(sand)
+        shale_values.append(shale)
+
+    df_dict = dict(Sand=sand_values, Shale=shale_values)
+    df = pd.DataFrame.from_dict(df_dict, orient='columns')
+
+    # Boxplot
+    bp = ax[0].boxplot(df, labels=["Sandstone", "Shale"],
+                       patch_artist=True, showfliers=False)
+    ax[0].set_ylabel('Percentage (%)')
+    ax[0].set_xlabel('Category')
+    ax[0].set_title('Training Images proportions (Traditional workflow)')
+
+    for element in ['boxes', 'whiskers', 'fliers', 'means', 'medians', 'caps']:
+        plt.setp(bp[element], color="black")
+
+    for patch in bp['boxes']:
+        patch.set(facecolor='#f1f1f1')
+
+    sand_values = list()
+    shale_values = list()
+
+    # Get proportions for fake data
+    for i in range(1, len(fake)):
+        sand, shale = get_sand_shale_proportion(fake[i].reshape(-1))
+        sand_values.append(sand)
+        shale_values.append(shale)
+
+    df_dict = dict(Sand=sand_values, Shale=shale_values)
+    df = pd.DataFrame.from_dict(df_dict, orient='columns')
+
+    # Boxplot
+    bp = ax[1].boxplot(df, labels=["Sandstone", "Shale"],
+                       patch_artist=True, showfliers=False)
+
+    for element in ['boxes', 'whiskers', 'fliers', 'means', 'medians', 'caps']:
+        plt.setp(bp[element], color="black")
+
+    for patch in bp['boxes']:
+        patch.set(facecolor='#f1f1f1')
+
+    ax[0].set_ylim(20, 80)
+    ax[1].set_ylim(20, 80)
+
+    ax[1].set_ylabel('Percentage (%)')
+    ax[1].set_xlabel('Category')
+    ax[1].set_title('Training Images proportions (Proposed workflow)')
+
+    plt.savefig("data/results/boxplot_snesim.png", dpi=600, bbox_inches='tight')
+
+def read_conditional_samples(filename: object = 'eas.dat', nanval: object = -997799) -> object:
+    debug_level = 1
+    if not (os.path.isfile(filename)):
+        print("Filename:'%s', does not exist" % filename)
+
+    file = open(filename, "r")
+    if debug_level > 0:
+        print("eas: file ->%20s" % filename)
+
+    eas = {'title': (file.readline()).strip('\n')}
+
+    if debug_level > 0:
+        print("eas: title->%20s" % eas['title'])
+
+    dim_arr = eas['title'].split()
+    if len(dim_arr) == 3:
+        eas['dim'] = {}
+        eas['dim']['nx'] = int(dim_arr[0])
+        eas['dim']['ny'] = int(dim_arr[1])
+        eas['dim']['nz'] = int(dim_arr[2])
+
+    eas['n_cols'] = int(file.readline())
+
+    eas['header'] = []
+    for i in range(0, eas['n_cols']):
+        # print (i)
+        h_val = (file.readline()).strip('\n')
+        eas['header'].append(h_val)
+
+        if debug_level > 1:
+            print("eas: header(%2d)-> %s" % (i, eas['header'][i]))
+
+    file.close()
+
+    try:
+        eas['D'] = np.genfromtxt(filename, skip_header=2 + eas['n_cols'])
+        if debug_level > 1:
+            print("eas: Read data from %s" % filename)
+    except:
+        print("eas: COULD NOT READ DATA FROM %s" % filename)
+
+    # add NaN values
+    try:
+        eas['D'][eas['D'] == nanval] = np.nan
+    except:
+        print("eas: FAILED TO HANDLE NaN VALUES (%d(" % nanval)
+
+    # If dimensions are given in title, then convert to 2D/3D array
+    if "dim" in eas:
+        if eas['dim']['nz'] == 1:
+            eas['Dmat'] = eas['D'].reshape((eas['dim']['ny'], eas['dim']['nx']))
+        elif eas['dim']['nx'] == 1:
+            eas['Dmat'] = np.transpose(eas['D'].reshape((eas['dim']['nz'], eas['dim']['ny'])))
+        elif eas['dim']['ny'] == 1:
+            eas['Dmat'] = eas['D'].reshape((eas['dim']['nz'], eas['dim']['nx']))
+        else:
+            eas['Dmat'] = eas['D'].reshape((eas['dim']['nz'], eas['dim']['ny'], eas['dim']['nx']))
+
+        eas['Dmat'] = eas['D'].reshape((eas['dim']['nx'], eas['dim']['ny'], eas['dim']['nz']))
+        eas['Dmat'] = eas['D'].reshape((eas['dim']['nz'], eas['dim']['ny'], eas['dim']['nx']))
+
+        eas['Dmat'] = np.transpose(eas['Dmat'], (2, 1, 0))
+
+        if debug_level > 0:
+            print("eas: converted data in matrixes (Dmat)")
+
+    eas['filename'] = filename
+
+    return eas
---
+++ generative_model/sampling.py
@@ -0,0 +1,272 @@
+# Unconditional sampling from Generator network
+
+import os
+import re
+import cv2
+import torch
+import random
+import argparse
+import pygeostat as gs
+import numpy as np
+import torch.nn as nn
+
+from tqdm import tqdm
+from graphics.plot import *
+from torch.autograd import Variable
+
+def get_args():
+    parser = argparse.ArgumentParser(description="Easily samples images from the Generator network!")
+
+    parser.add_argument("--model_path", default="Epoch_155.ckpt", type=str, help="Generator model path")
+    parser.add_argument("--num_samples", default=100, type=int, help="Number of samples to be generated")
+    
+    args = parser.parse_args()
+    return args
+
+
+def seeds(initial_seed: int) -> None:
+    os.environ['PYTHONHASHSEED'] = str(initial_seed)
+
+    # Torch RNG
+    torch.manual_seed(initial_seed)
+    torch.cuda.manual_seed(initial_seed)
+    torch.cuda.manual_seed_all(initial_seed)
+
+    # Python RNG
+    np.random.seed(initial_seed)
+    random.seed(initial_seed)
+
+
+def load_model(latent_space:int , state_dictionary: dict) -> nn.Module:
+    model = GeneratorModel(latent_size)
+    model.load_state_dict(state_dictionary["Generator"])
+    return model
+
+
+class Unconditional(object):
+    def __init__(self, dimension, generator, latent_size, latent_dist, use_cuda=False):
+        self.use_cuda = use_cuda
+
+        self.dimension = dimension
+        self.generator = generator
+
+        if self.use_cuda:
+            self.generator = self.generator.cuda()
+
+        self.latent_size = latent_size
+        self.latent_dist = latent_dist
+
+    def sample_batch(self, batch, m):
+        config = [batch, self.latent_size]
+        zhat = None
+        if self.use_cuda:
+            zhat = Variable(torch.FloatTensor(*config).cuda(), requires_grad=True)
+        else:
+            zhat = Variable(torch.FloatTensor(*config), requires_grad=True)
+        zhat.retain_grad()
+        if self.latent_dist == "normal":
+            zhat.data = zhat.data.normal_(0, 1)
+        elif self.latent_dist == "uniform":
+            zhat.data = zhat.data.uniform_(-1, 1)
+        return zhat
+
+    def create_unconditional_simulations(self, batch, m):
+        zhat = self.sample_batch(batch, m)
+        if self.use_cuda:
+            zhat = zhat.cuda()
+        try:
+            xhat = self.generator(zhat)
+            return xhat.data.cpu().numpy()
+        except RuntimeError as reError:
+            print(reError)
+
+
+class GeneratorModel(nn.Module):
+    """
+    Generator model for WGAN-GP.
+
+    Given a vector of random values (latent inputs) as input, this network
+    generates data with the same structure as the training data.
+    The goal is to train the generator to generate data that "fools" the
+    Critic.
+    """
+
+    def __init__(self, dim_in, dim: int = 128):
+        super(GeneratorModel, self).__init__()
+
+        def genblock(dim_in, dim_out):
+            
+            block = nn.Sequential(nn.ConvTranspose2d(in_channels=dim_in,
+                                                     out_channels=dim_out,
+                                                     kernel_size=5,
+                                                     stride=2,
+                                                     padding=2,
+                                                     output_padding=1,
+                                                     bias=False),
+                                  nn.BatchNorm2d(dim_out),
+                                  nn.ReLU()
+                                  )
+            return block
+
+        def genimg(dim_in):
+            block = nn.Sequential(nn.ConvTranspose2d(in_channels=dim_in,
+                                                     out_channels=1,
+                                                     kernel_size=5,
+                                                     stride=2,
+                                                     padding=2,
+                                                     output_padding=1,
+                                                     ),
+                                  nn.Tanh()
+                                  )
+            return block
+
+        self.prepare = nn.Sequential(nn.Linear(dim_in, dim*8*4*4, bias=False),
+                                     nn.BatchNorm1d(dim*8*4*4),
+                                     nn.ReLU())
+        self.generate = nn.Sequential(genblock(dim*8, dim*16),
+                                      genblock(dim*16, dim*8),
+                                      genblock(dim*8, dim*4),
+                                      genblock(dim*4, dim*2),
+                                      genimg(dim*2))
+
+    def forward(self, x):
+        """Forward pass function."""
+        x = self.prepare(x)
+        x = x.view(x.size(0), -1, 4, 4)
+        x = self.generate(x)
+        return x
+
+
+def create_out_file(samples):
+    for image in tqdm(samples,
+                    desc="Saving samples from GAN, please wait...",
+                    total=len(samples), colour='blue'):
+        resized_image = cv2.resize(image, (150, 150))
+        binarized_image = np.where(resized_image > 0.5, 1, 0)
+        numpy_tensor = binarized_image.squeeze().ravel()
+        try:
+            # Loads file and save with new image
+            dataset = np.loadtxt(f"data/generated.out")
+            new_TI = np.column_stack((dataset, numpy_tensor))
+            np.savetxt(fname = "data/generated.out", X = new_TI, 
+                        newline = os.linesep,
+                        header=f"150 150 1\n1\nfacies\n")
+            new_TI = np.column_stack((dataset, numpy_tensor))
+        
+        except FileNotFoundError:
+                np.savetxt(fname = f"data/generated.out",
+                        X=numpy_tensor,
+                        newline = os.linesep,
+                        header=f"150 150 1\n1\nfacies\n")
+    
+    # Saves 
+    numpy_array = np.loadtxt("data/generated.out")
+    header_name = [f'ti_{idx}' for idx in range(numpy_array.reshape(100, 150, 150).shape[0])]
+    header_name = f'\n'.join(header_name)
+
+    # Save again with header
+    np.savetxt(fname="data/generated.out",
+                X=numpy_array,
+                header=f"gan\n{numpy_array.reshape(100, 150, 150).shape[1]}\n{header_name}", comments="")
+
+
+def __create_folders():
+    os.makedirs("data/out_files", exist_ok=True)
+    os.makedirs("data/parfiles", exist_ok=True)
+    os.makedirs("data/simulations", exist_ok=True)
+    os.makedirs("data/results", exist_ok=True)
+
+
+def create_ti_files(samples):
+    __create_folders()
+    for idx, im in enumerate(samples):
+        im = cv2.resize(im, (150, 150))
+        image_resized = np.where(im > 0.5, 1, 0)
+
+        # save as .out
+        np.savetxt(f"data/out_files/ti_{idx}.out", image_resized.reshape(-1), header="150 150 1\n1\nfacies", fmt="%1d",comments='')
+
+
+def _execute_script(index: int):
+    os.chdir("data/")
+    script = f"""echo 'parfiles/snesim_gan_1.par' | wine snesim.exe"""
+    os.system("bash -c '%s'" % script)
+
+
+def simulate(samples_array):
+    seed_initial: int = 69069
+    n_reals = 100
+    seeds_list = gs.rseed_list(nseeds=n_reals, seed=seed_initial)
+
+    for idx, image in enumerate(samples_array):
+        image = cv2.resize(image, (150, 150))
+        image = (image > 0.5)
+
+        with open("data/gan.par", 'r') as parfile:
+            par = parfile.read()
+            par_replaced = par.replace(f"_0.out", f"_{idx}.out")
+            par_ready = re.sub("\d{5,6}", f"{seeds_list[idx]}", par_replaced)
+            
+            with open(f"data/parfiles/snesim_gan_{idx}.par", 'w+') as file:
+                file.write(par_ready)
+        _execute_script(idx)
+        os.chdir("..")
+
+def plots():
+    print("[INFO] Loading simulations", end="\r")
+    realizations = concatenate_out_files("data/simulations/")
+
+    ti_dict = dict()
+    for idx, realization in enumerate(realizations):
+        ti_dict[f'ti_{idx+1}'] = np.array(realization.reshape(-1))
+
+    dataframe = pd.DataFrame(ti_dict)
+    plot_uncertainty(dataframe)
+    plot_realizations_grid(realizations)
+
+    file = read_conditional_samples("../snesim/data/snesim.out")["D"]
+    realizations = file[:, 0].reshape(100, 150, 150)
+
+    print("[INFO] Loading simulations", end="\r")
+    snesim_gen_realizations = concatenate_out_files("data/simulations/")
+
+    proportions_comparison(realizations,
+                       snesim_gen_realizations)
+
+if __name__ == "__main__":
+    seeds(69069)
+
+    param = get_args()
+
+    latent_size = 100
+    state_dict = torch.load(param.model_path, map_location='cpu')
+
+    generator = load_model(latent_size, state_dict)
+
+    uncond = Unconditional(1, generator, latent_size, "normal")
+
+    if os.listdir("data/out_files") is None:
+        if param.num_samples<=100:
+            print("[INFO] Sampling images from GAN")
+            for i in range(1):
+                samples = uncond.create_unconditional_simulations(100, [i, i, i])
+            samples_arr = np.where(np.concatenate(samples, 0)*0.5+0.5 >= 0.5, 1.0, 0.0)
+        else:
+            pass
+
+        print("[INFO] Images sampled!")
+        create_out_file(samples_arr)
+
+        # Performs SNESIM simulations
+        # Prepare each TI
+        create_ti_files(samples_arr)
+
+    if os.listdir("data/simulations") is None:
+        # Simulate
+        simulate(samples_arr)
+    
+    # Starts plotting
+    os.makedirs("data/results", exist_ok=True)
+    plots()
+    
+
